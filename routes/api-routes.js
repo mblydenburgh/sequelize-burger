11e3c9d642189ac477a7549244e4461f8ed86f43
Michael Blydenburgh <mblydenburgh@icloud.com>
2019-01-08 14:40:49 +0000

    insert burger working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,10 +2,33 @@ const db = require('../models');
 
 module.exports = function(app){
     
+    // GET Request to load burger data from database
     app.get('/', async (req,res)=>{
         const data = await db.burgers.findAll({});
         // console.log(data);
         res.render("index",{burgers:data});
     });
+    
+    // POST Request to add new burger
+    app.post('/', async (req,res)=>{
+        const newName = req.body.name;
+        const eaten = false;
+        // console.log(newName,eaten);
+        const data = await db.burgers.create({
+          name: newName,
+          eaten: eaten
+        });
+        
+        res.redirect('/');
+    });
+    
+    
+    // PUT Request to update burger state
+    
+    
+    
+    // DELETE Request to delte burger from database
+    
+    
     
 };
